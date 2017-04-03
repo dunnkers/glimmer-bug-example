d7b9884e78533c4342ff549aecb9b41c2373bbbd
Dunnkers <dunnkers@hotmail.com>
2017-04-03 14:35:03 +0200

    Add a relevant test

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from 'glimmer-bug-example/tests/helpers/module-for-acceptance';
 
@@ -8,5 +9,29 @@ test('visiting /index', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/');
+  });
+
+  andThen(function() {
+    let done = assert.async();
+
+    let testElement = () => {
+      let button = document.querySelector('paper-button');
+
+      assert.ok(button.shadowRoot || button.shadyRoot,
+        'paper-button has shadowRoot');
+
+      assert.equal(Ember.$('paper-button').attr('role'), 'button',
+        'paper-button rendered successfully');
+
+      done();
+    };
+
+    if (window.Polymer && (window.Polymer.Settings.nativeShadow ||
+        window.Polymer.RenderStatus.hasRendered())) {
+      testElement();
+    } else {
+      // wait for elements to be polyfilled
+      window.addEventListener('WebComponentsReady', testElement);
+    }
   });
 });
